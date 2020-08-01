6bfc0882423ce459e5f4c2955bcd12f30942258f
praveensam <praveensams16@gmail.com>
2020-08-02 00:42:06 +0300

    uploads

---
+++ ec2/main.tf
@@ -0,0 +1,17 @@
+resource "aws_network_interface" "foo" {
+  subnet_id   = var.subnet
+
+  tags = {
+    Name = "primary_network_interface"
+  }
+}
+
+resource "aws_instance" "foo" {
+  ami           = "ami-02354e95b39ca8dec" # us-west-2
+  instance_type = "t2.micro"
+
+  network_interface {
+    network_interface_id = aws_network_interface.foo.id
+    device_index         = 0
+  }
+}
